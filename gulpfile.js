f77e4338e027939a9eafc24acf02fa3d532332c7
Jeremy Zilar <jeremyzilar@gmail.com>
2017-07-31 18:00:42 -0400

    cleaning up pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -6,59 +6,60 @@ var gulp         = require("gulp"),
     concat    = require('gulp-concat'),
     cleanCSS     = require('gulp-clean-css')
 
-
-// Compile SCSS files to CSS
-gulp.task("scss", function () {
-  
-  //Delete our old css files
-  del(["themes/digital.gov/static/css/**/*"])
-
-  //take care of the CSS files
-  gulp.src('themes/digital.gov/src/css/**/*.css')
-    .pipe(concat("style.min.css"))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('themes/digital.gov/static/css'))
-    .pipe(hash.manifest("css.json"))
-    .pipe(gulp.dest("data/css"))
-
-  gulp.src("themes/digital.gov/src/scss/**/*.scss")
-    .pipe(sass({
-      outputStyle : "compressed"
-    }))
-    .pipe(autoprefixer({
-      browsers : ["last 20 versions"]
-    }))
-    .pipe(hash())
-    .pipe(gulp.dest("themes/digital.gov/static/css"))
-    .pipe(hash.manifest("sass.json"))
-    .pipe(gulp.dest("data/css"))
-})
-
-// Hash javascript
-gulp.task("js", function () {
-  del(["themes/digital.gov/static/js/**/*"])
-  gulp.src("themes/digital.gov/src/js/**/*")
-    .pipe(concat('all.js'))
-    .pipe(hash())
-    .pipe(gulp.dest("themes/digital.gov/static/js"))
-    .pipe(hash.manifest("hash.json"))
-    .pipe(gulp.dest("data/js"))
-})
-
 // Hash images
 gulp.task("images", function () {
-  del(["static/images/**/*"])
-  gulp.src("src/images/**/*")
-      .pipe(hash())
-      .pipe(gulp.dest("static/images"))
+  del(["content/aws/**/*"])
+  gulp.src("content/media/**/*")
+    .pipe(hash())
+    .pipe(hash.manifest("./data/media.json"))
+    .pipe(gulp.dest("content/aws/"))
 })
+
+
+// // Compile SCSS files to CSS
+// gulp.task("scss", function () {
+//
+//   //Delete our old css files
+//   del(["themes/digital.gov/static/css/**/*"])
+//
+//   //take care of the CSS files
+//   gulp.src('themes/digital.gov/src/css/**/*.css')
+//     .pipe(concat("style.min.css"))
+//     .pipe(cleanCSS())
+//     .pipe(gulp.dest('themes/digital.gov/static/css'))
+//     .pipe(hash.manifest("css.json"))
+//     .pipe(gulp.dest("data/css"))
+//
+//   gulp.src("themes/digital.gov/src/scss/**/*.scss")
+//     .pipe(sass({
+//       outputStyle : "compressed"
+//     }))
+//     .pipe(autoprefixer({
+//       browsers : ["last 20 versions"]
+//     }))
+//     .pipe(hash())
+//     .pipe(gulp.dest("themes/digital.gov/static/css"))
+//     .pipe(hash.manifest("sass.json"))
+//     .pipe(gulp.dest("data/css"))
+// })
+//
+// // Hash javascript
+// gulp.task("js", function () {
+//   del(["themes/digital.gov/static/js/**/*"])
+//   gulp.src("themes/digital.gov/src/js/**/*")
+//     .pipe(concat('all.js'))
+//     .pipe(hash())
+//     .pipe(gulp.dest("themes/digital.gov/static/js"))
+//     .pipe(hash.manifest("hash.json"))
+//     .pipe(gulp.dest("data/js"))
+// })
+
+
 
 // ================
 // Watch asset folder for changes
-gulp.task("watch", ["scss", "images", "js"], function () {
-  gulp.watch("themes/digital.gov/src/scss/**/*", ["scss"])
-  gulp.watch("themes/digital.gov/src/images/**/*", ["images"])
-  gulp.watch("themes/digital.gov/src/js/**/*", ["js"])
+gulp.task("watch", ["images"], function () {
+  gulp.watch("content/media/**/*", ["images"])
 })
 
 // Set watch as default task
